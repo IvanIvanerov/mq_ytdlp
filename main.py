6c42bdaec5102199a8421210421a045ff4b6b404
IvanIvanerov <ivanero123@gmail.com>
2022-09-28 06:38:48 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ MQ_TOPIC = sys.argv[2]
 MQ_SERVER = sys.argv[1]
 MQ_PORT = 1883
 ydl_opts = {
-        'format': 'bestvideo[height<=1080]/best[ext=mp4]/best',
+        'format': 'best[height=1080][ext=mp4]/best[height<=1080][ext=mp4]/best',
         'outtmpl': sys.argv[3]+'/%(uploader)s/%(title)s.%(ext)s',
         'cachedir': False,
         'logtostderr': True
